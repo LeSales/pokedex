744d906543164883d9fd515225e91299f221852d
Leandro Sales <leandrossales.96@gmail.com>
2021-08-24 13:27:30 -0300

    Add selection in list items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/models/pokemon.dart';
 import 'package:pokedex/repositories/pokemon_repository.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final pokemons = PokemonRepository.pokemons;
+  List<Pokemon> selecionados = [];
+
   @override
   Widget build(BuildContext context) {
-    final pokemons = PokemonRepository.pokemons;
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Pokédex'),
@@ -13,9 +22,36 @@ class HomePage extends StatelessWidget {
         body: ListView.separated(
           itemBuilder: (BuildContext context, int pokemon) {
             return ListTile(
-              leading: Image.asset(pokemons[pokemon].icon),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              leading: (selecionados.contains(pokemons[pokemon]))
+                  ? CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : SizedBox(
+                      child: Image.asset(pokemons[pokemon].icon),
+                      width: 40,
+                    ),
               title: Text(pokemons[pokemon].name),
               trailing: Text(pokemons[pokemon].type1),
+              selected: selecionados.contains(pokemons[pokemon]),
+              selectedTileColor: Colors.red[50],
+              onLongPress: () {
+                setState(() {
+                  (selecionados.contains(pokemons[pokemon]))
+                      ? selecionados.remove(pokemons[pokemon])
+                      : selecionados.add(pokemons[pokemon]);
+                });
+              },
+              onTap: () {
+                (selecionados.length == 0)
+                    ? print("To do: go to description")
+                    : setState(() {
+                        (selecionados.contains(pokemons[pokemon]))
+                            ? selecionados.remove(pokemons[pokemon])
+                            : selecionados.add(pokemons[pokemon]);
+                      });
+              },
             );
           },
           padding: EdgeInsets.all(16),
