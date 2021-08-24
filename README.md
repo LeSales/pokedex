bf2f25825efd52b03b985d1ed640a6f996f3f7a0
Leandro Sales <leandrossales.96@gmail.com>
2021-08-24 12:07:15 -0300

    create a static pokemon list

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Pokédex
 
-Pokédex é um aplicativo que te permite consultar todos os Pokémons existentes, registrar os Pokémons avistados e gerenciar todos os Pokémons capturados 
+Pokédex é um aplicativo para consultar todos os Pokémons existentes, registrar os Pokémons avistados e gerenciar todos os Pokémons capturados 
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/repositories/pokemon_repository.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final pokemons = PokemonRepository.pokemons;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pokédex'),
-      ),
-      body: Center(
-        child: Text('To do: Home Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Pokédex'),
+        ),
+        body: ListView.separated(
+          itemBuilder: (BuildContext context, int pokemon) {
+            return ListTile(
+              leading: Image.asset(pokemons[pokemon].icon),
+              title: Text(pokemons[pokemon].name),
+              trailing: Text(pokemons[pokemon].type1),
+            );
+          },
+          padding: EdgeInsets.all(16),
+          separatorBuilder: (_, __) => Divider(),
+          itemCount: pokemons.length,
+        ));
   }
 }
